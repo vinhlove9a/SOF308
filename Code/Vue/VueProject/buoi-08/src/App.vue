db744c9fbdf8d3bcfab6886e968f4acd5fa904da
vinhlove9a <beochy204@gmail.com>
2025-03-28 07:37:12 +0700

    add b8

--- Code/Vue/VueProject/buoi-08/src/App.vue
+++ Code/Vue/VueProject/buoi-08/src/App.vue
@@ -1,126 +1,68 @@
 <template>
-  <div class="title">
-    <h2>LESSON 8</h2>
-    <div class="bai1">
-      <!-- Tao 1  doi tuong sanPham gom cac thuoc tinh
-    id - number, ten - String, mauSac - String, gia - number
-    Hien thi danh sach doi tuong san pham theo cac dinh dang sau
-    1. Hien thi duoi dang table
-    2. Hien thi duoi dang cbb
-    3. Hien thi duoi dang ul, li
-    --></div>
-    <div class="viewTable">
-      <h3>View Table</h3>
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">ten</th>
-            <th scope="col">mau sac</th>
-            <th scope="col">gia</th>
-          </tr>
-        </thead>
-        <tbody v-for="(sp, index) in sanPhams" :key="index">
-          <tr>
-            <th scope="row">{{ sp.id }}</th>
-            <td>{{ sp.ten }}</td>
-            <td>{{ sp.mauSac }}</td>
-            <td>{{ sp.gia }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="viewCBB">
-      <h3>View CBB</h3>
-      <select class="form-select" aria-label="Default select example">
-        <option v-for="(sp, index) in sanPhams" :key="index">
-          {{ sp.ten }}
-        </option>
-      </select>
-    </div>
-    <div class="viewUL_LI">
-      <ul>
-        <li v-for="(sp, index) in sanPhams" :key="index">
-          <p>{{ sp.id }}</p>
-          <p>{{ sp.ten }}</p>
-          <p>{{ sp.mauSac }}</p>
-          <p>{{ sp.gia }}</p>
-        </li>
-      </ul>
-    </div>
-    <div class="checkLogin">
-      <p>{{ isLogin ? `Da dang nhap` : `Chua dang nhap` }}</p>
-      <button @click="checkLogin">Swap</button>
-    </div>
-    <div class="changeStatus">
-      <p>{{ status }}</p>
-      <button @click="changeStatus">Change</button>
-    </div>
-  </div>
-  <div class="changeStatus2">
-    <p v-if="status === 'active'">active</p>
-    <p v-else-if="status === 'away'">away</p>
-    <p v-else>busy</p>
-    <!-- <p>{{ status === 'active' ? 'Trang thai hoat dong' : status === 'away' ? 'Trang thai vang mat' : 'Trang thai ban' }}</p> -->
-  </div>
-  <div class="caculator">
-    <input type="number" v-model="number1" />
-    <input type="number" v-model="number2" />
-    <button @click="tinhSum">Sum</button>
-    <button @click="tinhSub">Sub</button>
-    <p>Sum: {{ sum }}</p>
-    <p>Sub: {{ sub }}</p>
+  <!-- <div class="title">
+    <h1>LESSON 8</h1>
+  </div> -->
+  <div class="tblNhanVien">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Tên</th>
+          <th scope="col">Tuổi</th>
+          <th scope="col">Giới tính</th>
+          <th scope="col">EXP Years</th>
+        </tr>
+      </thead>
+      <tbody v-for="nhanVien in nhanViens" :key="nhanVien.ten">
+        <tr>
+          <td>{{ nhanVien.ten }}</td>
+          <td>{{ nhanVien.tuoi }}</td>
+          <td>{{ nhanVien.gioiTinh ? "Nam" : "Nữ" }}</td>
+          <td>{{ nhanVien.soNamLamViec }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
 <script setup>
-const number1 = ref(0);
-const number2 = ref(0);
-const sum = ref(0);
-const sub = ref(0);
-function tinhSum() {
-  sum.value = number1.value + number2.value;
-}
-function tinhSub() {
-  sub.value = number1.value - number2.value;
-}
-
 import { ref } from "vue";
-
-const sanPhams = ref([
-  { id: 1, ten: "Iphone 12", mauSac: "Den", gia: 1000 },
-  { id: 2, ten: "Iphone 11", mauSac: "Trang", gia: 900 },
-  { id: 3, ten: "Iphone 10", mauSac: "Xanh", gia: 800 },
-  { id: 4, ten: "Iphone 9", mauSac: "Do", gia: 700 },
-  { id: 5, ten: "Iphone 8", mauSac: "Vang", gia: 600 },
+const nhanViens = ref([
+  {
+    ten: "Nguyen Van A",
+    tuoi: 20,
+    gioiTinh: true,
+    soNamLamViec: 5,
+  },
+  {
+    ten: "Nguyen Van B",
+    tuoi: 25,
+    gioiTinh: false,
+    soNamLamViec: 3,
+  },
+  {
+    ten: "Nguyen Van C",
+    tuoi: 30,
+    gioiTinh: true,
+    soNamLamViec: 10,
+  },
+  {
+    ten: "Nguyen Van D",
+    tuoi: 35,
+    gioiTinh: false,
+    soNamLamViec: 7,
+  },
+  {
+    ten: "Nguyen Van E",
+    tuoi: 40,
+    gioiTinh: true,
+    soNamLamViec: 15,
+  },
 ]);
-
-const isLogin = ref(false);
-function checkLogin() {
-  isLogin.value = !isLogin.value;
-}
-
-const status = ref("active");
-function changeStatus() {
-  if (status.value === "active") {
-    status.value = "away";
-  } else if (status.value === "away") {
-    status.value = "busy";
-  } else {
-    status.value = "active";
-  }
-}
 </script>
+
 <style>
-.viewTable h3 {
-  margin-top: 20px;
-  color: red;
-}
-.viewCBB h3 {
-  margin-top: 20px;
-  color: red;
-}
-.viewUL_LI h3 {
-  margin-top: 20px;
+h1 {
+  text-align: center;
   color: red;
 }
 </style>
--- Code/Vue/VueProject/buoi-08/src/main.js
+++ Code/Vue/VueProject/buoi-08/src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-createApp(App).mount("#app");
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+createApp(App).mount('#app')
